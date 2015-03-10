f41f14325e52fd4e38d033cbad54f5bf534fd25b
GongT <gongteng524702837@gmail.com>
2015-03-11 04:05:46 +0800

    fix bugs

--- scripts/avrun.js
+++ scripts/avrun.js
@@ -18,10 +18,12 @@ module.exports = function (){
 		});
 	});
 };
+
+var platformExec;
 module.exports.external = function (cmd){
 	return new Promise(function (resolve, reject){
 		console.log('start external program - %s', cmd);
-		var p = childProcess.exec(cmd);
+		var p = platformExec(cmd);
 		p.on("exit", function (code){
 			if(code == 0){
 				resolve();
@@ -41,15 +43,17 @@ if(!module.exports.runner){
 
 if(process.env.comspec){
 	module.exports.spawn = spawn_windows;
+	platformExec = exec_windows;
 } else{
 	module.exports.spawn = spawn_linux;
+	platformExec = exec_linux;
 }
 
 function spawn_windows(args, options){
 	if(!args){
 		args = [];
 	}
-	var args = ['/C', 'node', module.exports.runner].concat(args);
+	args = ['/C', 'node', module.exports.runner].concat(args);
 	
 	console.log('call lean-cloud script - %s %s.', process.env.comspec, args.join(' '));
 	return childProcess.spawn(process.env.comspec, args, options);
@@ -58,4 +62,19 @@ function spawn_windows(args, options){
 function spawn_linux(args, options){
 	console.log('call lean-cloud script - %s %s.', module.exports.runner, args.join(' '));
 	return childProcess.spawn(module.exports.runner, args, options);
+}
+
+function exec_windows(args, options){
+	if(!args){
+		args = [];
+	}
+	args = ['/C', 'node', module.exports.runner].concat(args);
+	
+	console.log('exec external file - %s %s.', process.env.comspec, args.join(' '));
+	return childProcess.exec(process.env.comspec, args, options);
+}
+
+function exec_linux(args, options){
+	console.log('exec external file - %s %s.', module.exports.runner, args.join(' '));
+	return childProcess.exec(module.exports.runner, args, options);
 }
--- scripts/resolve_dependence.js
+++ scripts/resolve_dependence.js
@@ -1,2 +1,2 @@
 console.log('Lean-G: resolve dependence -> run npm install');
-var p = require('./avrun.js').spawn('npm', ['install'], {stdio: "inherit"});
+var p = require('./avrun.js').external('npm', ['install'], {stdio: "inherit"});
