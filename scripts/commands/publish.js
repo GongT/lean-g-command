cdb82ca8e128446468906fb4f438b3d9ca3ad4cc
GongT <gongteng524702837@gmail.com>
2015-03-13 15:12:23 +0800

    优惠券激活码没加类型过滤

---
+++ scripts/commands/publish.js
@@ -0,0 +1,10 @@
+/**
+ * @title: 部署测试环境代码到发布环境
+ */
+var avosrun = require('../avrun.js');
+
+avosrun('app').then(function (){
+	return avosrun('publish');
+}).then(null, function (){
+	console.log.apply(console.log, arguments);
+});
