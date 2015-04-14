1c2da7864cf8bafb2b1970e66d01f34784dc6ede
GongT <gongteng524702837@gmail.com>
2015-04-14 22:03:44 +0800

    压缩json在部署环境

--- view_functions/json.js
+++ view_functions/json.js
@@ -1,5 +1,13 @@
-module.exports = function (params){
-	var data = params.__get(false, null, 0);
-	// console.log('call %s(%s)', fn, args.join(', '));
-	return JSON.stringify(data, null, 8);
-};
+if(AV.isDebugEnv){
+	module.exports = function (params){
+		var data = params.__get(false, null, 0);
+		// console.log('call %s(%s)', fn, args.join(', '));
+		return JSON.stringify(data, null, 8);
+	};
+}else{
+	module.exports = function (params){
+		var data = params.__get(false, null, 0);
+		// console.log('call %s(%s)', fn, args.join(', '));
+		return JSON.stringify(data);
+	};
+}
