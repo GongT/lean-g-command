12d36cf0364327e91fb360451fdbe6e1c267e3c0
GongT <gongteng524702837@gmail.com>
2015-04-24 17:00:05 +0800

    技师端企业单接口

--- scripts/commands/deploy.js
+++ scripts/commands/deploy.js
@@ -3,12 +3,17 @@
  */
 var avosrun = require('../avrun.js');
 var fs = require('fs');
+var Promise = require('promise');
 
 avosrun('app').then(function (){
 	require('../deploy_helper/modify_package');
 	return avosrun('deploy');
 }).then(function (){
-	return avosrun('publish');
+	return new Promise(function (resolve, reject){
+		setTimeout(function (){
+			return avosrun('publish').then(resolve, reject);
+		}, 3000);
+	})
 }).then(null, function (){
 	console.log.apply(console.log, arguments);
 });
