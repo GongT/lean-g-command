3e81d44bcb146698083584c890af5a36a97b1e2d
GongT <gongteng524702837@gmail.com>
2015-04-28 18:52:34 +0800

    阻止部署调试环境

--- scripts/commands/deploy.js
+++ scripts/commands/deploy.js
@@ -5,6 +5,11 @@ var avosrun = require('../avrun.js');
 var fs = require('fs');
 var Promise = require('promise');
 
+if(APP_CONFIG.blockDeploy){
+	console.error('\x1B[38;5;14m%s\x1B[0m不允许部署，它可能仅用于本地调试。', APP_CONFIG.applicationName);
+	process.exit(9);
+}
+
 avosrun('app').then(function (){
 	require('../deploy_helper/modify_package');
 	return avosrun('deploy');
