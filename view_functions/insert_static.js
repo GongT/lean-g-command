81cc6558f91ff2254b2cf3b8d8f3e86e3930cab9
GongT <gongteng524702837@gmail.com>
2015-05-15 15:11:32 +0800

    DataTables修改

--- view_functions/insert_static.js
+++ view_functions/insert_static.js
@@ -1,26 +1,75 @@
 // var AV = Object.AV;
-var CONSTANTS = AV.CONSTANTS;
-
+var minjs = AV.CONSTANTS.isDebugEnv? 'js' : 'min.js';
 var librarys = {
-	'jquery'     : ['jquery' + (CONSTANTS.isDebugEnv? '' : '.min') + '.js', 'jquery.cookie.js'],
-	'jquery-only': ['jquery' + (CONSTANTS.isDebugEnv? '' : '.min') + '.js'],
+	'jquery'     : ['jquery.' + minjs, 'jquery.cookie.js'],
+	'jquery-only': ['jquery.' + minjs],
 	'artDialog'  : ['artDialog.js', 'iframeTools.source.js', 'artdialog/default.css']
+};
+
+var plugins = {
+	'artDialog' : {
+		root      : 'artdialog',
+		files     : ['ui-dialog.css', 'dialog.' + minjs, 'dialog-plus.' + minjs],
+		dependence: ['jquery-only']
+	},
+	'dataTables': {
+		root      : 'datatable',
+		files     : ['jquery.dataTables.js', 'jquery.dataTables.css', 'jquery.dataTables.extend.js'],
+		dependence: ['jquery-only', 'ajax-form.js']
+	}
 };
 
 var isJs = /\.js$/;
 var isCss = /\.css$/;
 
+function no_duplicate_append(data, file){
+	if(!data._static_dup){
+		data._static_dup = {};
+	}
+	if(data._static_dup[file]){
+		return;
+	}
+	data._static_dup[file] = true;
+	if(isJs.test(file)){
+		data._head.ascripts.push(wrapjs(file));
+	} else if(isCss.test(file)){
+		data._head.astyles.push(wrapcss(file));
+	} else{
+		data._head.ascripts.push(js_alert_error('没有找到引入的静态文件：' + file));
+	}
+}
+function no_duplicate_prepend(data, file){
+	if(!data._static_dup){
+		data._static_dup = {};
+	}
+	if(data._static_dup[file]){
+		return;
+	}
+	data._static_dup[file] = true;
+	if(isJs.test(file)){
+		data._head.pscripts.push(wrapjs(file));
+	} else if(isCss.test(file)){
+		data._head.pstyles.push(wrapcss(file));
+	} else{
+		data._head.pscripts.push(js_alert_error('没有找到引入的静态文件：' + file));
+	}
+}
+
 function insert_static(params, data){
 	var file = params.__get('file', '', 0);
 	var external = params.__get('external');
 	var library = params.__get('library');
+	var plugin = params.__get('plugin');
 	if(file){
 		if(isJs.test(file)){
-			data._head.ascripts.push(wrapjs('scripts/' + file));
+			no_duplicate_append(data, 'scripts/' + file);
 		} else if(isCss.test(file)){
-			data._head.astyles.push(wrapcss('styles/' + file));
+			no_duplicate_append(data, 'styles/' + file);
 		} else if(librarys[file]){
 			library = file;
+			file = null;
+		} else if(plugins[file]){
+			plugin = file;
 			file = null;
 		} else{
 			return js_alert_error('没有找到引入的静态文件：' + file);
@@ -35,11 +84,26 @@ function insert_static(params, data){
 			return js_alert_error('未知的静态文件库：' + library);
 		}
 	}
+	if(plugin){
+		if(plugins[plugin]){
+			plugin = plugins[plugin];
+			use_plugin(plugin.root, plugin.files, data);
+			if(plugin.dependence){
+				plugin.dependence.forEach(function (library){
+					if(librarys[library]){
+						use_library(librarys[library], data);
+					} else if(isJs.test(library) || isCss.test(library)){
+						use_library([library], data);
+					}
+				});
+			}
+		} else{
+			return js_alert_error('未知的插件：' + plugin);
+		}
+	}
 	if(external){
-		if(isJs.test(external)){
-			data._head.ascripts.push(wrapjs(external));
-		} else if(isCss.test(external)){
-			data._head.astyles.push(wrapcss(external));
+		if(isJs.test(external) || isCss.test(external)){
+			no_duplicate_append(data, external);
 		} else{
 			return js_alert_error('没有找到引入的静态文件：' + external);
 		}
@@ -49,22 +113,33 @@ function insert_static(params, data){
 function use_library(list, data){
 	list.forEach(function (file){
 		if(isJs.test(file)){
-			data._head.pscripts.push(wrapjs('js/' + file));
+			no_duplicate_prepend(data, 'js/' + file);
 		} else{
-			data._head.pstyles.push(wrapcss('css/' + file));
+			no_duplicate_prepend(data, 'css/' + file);
 		}
+	});
+}
+function use_plugin(name, list, data){
+	list.forEach(function (file){
+		var fp;
+		if(isJs.test(file)){
+			fp = 'plugin/' + name + '/js/' + file;
+		} else{
+			fp = 'plugin/' + name + '/css/' + file;
+		}
+		no_duplicate_append(data, fp);
 	});
 }
 
 function wrapjs(file){
 	file = file.replace(/^\//g, '');
-	return '<script type="text/javascript" crossorigin="anonymous" src="' + CONSTANTS.STATIC_URL + file + '?v=' +
-	       CONSTANTS.STATIC_VERSION + '"></script>';
+	return '<script type="text/javascript" crossorigin="anonymous" src="' + AV.CONSTANTS.STATIC_URL + file + '?v=' +
+	       AV.CONSTANTS.STATIC_VERSION + '"></script>';
 }
 function wrapcss(file){
 	file = file.replace(/^\//g, '');
-	return '<link type="text/css" rel="stylesheet" href="' + CONSTANTS.STATIC_URL + file + '?v=' +
-	       CONSTANTS.STATIC_VERSION + '">';
+	return '<link type="text/css" rel="stylesheet" href="' + AV.CONSTANTS.STATIC_URL + file + '?v=' +
+	       AV.CONSTANTS.STATIC_VERSION + '">';
 }
 function js_alert_error(text){
 	text = JSON.stringify(text);
