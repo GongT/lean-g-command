3113e7de9746c42c40fc80fe4bc91986d79ce6e3
GongT <gongteng524702837@gmail.com>
2015-05-19 14:49:31 +0800

    修复Date对象

--- include/debug-client/dummy_date.js
+++ include/debug-client/dummy_date.js
@@ -4,7 +4,7 @@ var allFunctionImpl = Object.keys(Date.prototype).every(function (fn){
 	return DummyDate.prototype[fn];
 });
 if(allFunctionImpl){
-	global.Date = DummyDate;
+	// global.Date = DummyDate;
 } else{
 	setTimeout(function (){
 		console.log('DummyDate\x1B[38;5;9mDummyDate - 伪装的Date对象无法使用，请检查。。\x1B[0m');
