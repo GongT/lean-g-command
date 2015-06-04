e234f2b1caac63e9a0765601fec6035b47e334c0
GongT <gongteng524702837@gmail.com>
2015-06-04 22:15:14 +0800

    rename to lean-g-template-nsmarty

--- commands/debug.js
+++ commands/debug.js
@@ -3,7 +3,7 @@
  * @windowTitle: 调试
  */
 var confirm_module = require('../lib/npm_installer').confirm;
-console.assert(confirm_module('leanengine-template-nsmarty', LEAN_G_INSTALL_FROM + 'leanengine-template-nsmarty.git'),
+console.assert(confirm_module('lean-g-template-nsmarty', LEAN_G_INSTALL_FROM + 'lean-g-template-nsmarty.git'),
 		'安装失败，请尝试手动安装');
 console.assert(confirm_module('lean-g-debugger-client', LEAN_G_INSTALL_FROM + 'lean-g-debugger-client.git'),
 		'安装失败，请尝试手动安装');
