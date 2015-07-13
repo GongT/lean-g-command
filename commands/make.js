0067927bd37ee8bc7cc1f87b51521225fe086523
GongT <gongteng524702837@gmail.com>
2015-07-13 18:16:41 +0800

    windows上的显示修复

--- commands/make.js
+++ commands/make.js
@@ -3,9 +3,15 @@
  * @windowTitle: 生成配置文件
  */
 
-var avosrun = require('../avrun.js');
+var confirm_module = require('../lib/npm_installer').confirm;
+console.assert(confirm_module('lean-g-template-nsmarty', LEAN_G_INSTALL_FROM + 'lean-g-template-nsmarty.git'),
+		'安装失败，请尝试手动安装');
+console.assert(confirm_module('lean-g-debugger-client', LEAN_G_INSTALL_FROM + 'lean-g-debugger-client.git'),
+		'安装失败，请尝试手动安装');
+console.assert(confirm_module('lean-g-loader', LEAN_G_INSTALL_FROM + 'lean-g-loader.git'),
+		'安装失败，请尝试手动安装');
 
-avosrun('app').then(function (){
-	console.log('生成完毕～');
-	process.exit(0);
-});
+require('lean-g-loader').createServerConfig();
+process.removeAllListeners('exit');
+console.log('make complete!');
+process.exit(0);
